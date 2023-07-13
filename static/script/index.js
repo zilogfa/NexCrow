f7beb4ee3caf4f6f7c95801be87f78c307ddd3e3
zilogfa <Zilogfa@live.com>
2023-07-13 07:55:47 -0700

    Like Script move to JS file

---
+++ static/script/index.js
@@ -0,0 +1,51 @@
+
+
+// -------------------- Like Script ♥   
+function likePost(event, button) {
+    event.preventDefault();
+
+    var postId = button.getAttribute("data-post-id");
+    var likesCountElement = button.querySelector(".likes-count");
+    var likesCount = parseInt(likesCountElement.textContent);
+
+    fetch(`/like/${postId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      if (response.ok) {
+        // Handle successful response
+        button.classList.toggle("liked");
+        if (button.classList.contains("liked")) {
+          likesCount++;
+        } else {
+          likesCount--;
+        }
+        likesCountElement.textContent = likesCount;
+        updateHeartEmoji(button, likesCount);
+      } else {
+        // Handle error response
+        console.error('Failed to like the post');
+      }
+    })
+    .catch(error => {
+      // Handle error
+      console.error('An error occurred:', error);
+    });
+  }
+
+  function updateHeartEmoji(button, likesCount) {
+    var heartEmoji = button.querySelector('.heart-emoji');
+    if (likesCount === 1) {
+      heartEmoji.innerHTML = '♥'; // Red heart
+      heartEmoji.classList.add('red-heart');
+      heartEmoji.classList.remove('empty-heart');
+    } else {
+      heartEmoji.innerHTML = '♥'; // Empty heart
+      heartEmoji.classList.remove('red-heart');
+      heartEmoji.classList.add('empty-heart');
+    }
+  }
+// -------------------- END Like Script ♥ 
