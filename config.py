f4d75af8ab8f6eb0e0e78dc7907f36b3de694903
zilogfa <Zilogfa@live.com>
2023-10-30 15:51:00 -0700

    Adding contents

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
+DATABASE_URL = ""
+
 class Config:
-    SECRET_KEY = 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', "8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL",  "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
     UPLOAD_FOLDER = 'static'
